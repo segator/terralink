7ab8ab34966399a5b12b98a8690f3a8e8433ae22
Isaac Aymerich <isaac.aymerich@roche.com>
2025-08-02 11:24:12 +0200

    ci: create GH action

--- github-action/install.sh
+++ github-action/install.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 set -e
-VERSION="${1:-0.2.1}"
+VERSION="${1}"
 OS="$(uname -s | tr '[:upper:]' '[:lower:]')"
 ARCH="$(uname -m)"
 case "$OS" in
   linux) OS="linux" ;;
   darwin) OS="darwin" ;;
-  msys*|cygwin*|mingw*|windows*) OS="windows" ;;
-  *) echo "Unsupported OS: $OS" && exit 1 ;;
 esac
 case "$ARCH" in
   x86_64) ARCH="amd64" ;;
   arm64|aarch64) ARCH="arm64" ;;
-  *) echo "Unsupported architecture: $ARCH" && exit 1 ;;
 esac
-EXT=""
-if [[ "$OS" == "windows" ]]; then
-  EXT=".exe"
-fi
-BINARY="terralink-${VERSION}-${OS}-${ARCH}${EXT}"
+
+BINARY="terralink-${VERSION}-${OS}-${ARCH}"
 URL="https://github.com/segator/terralink/releases/download/v${VERSION}/${BINARY}"
 echo "Downloading $URL"
-curl -L "$URL" -o terralink${EXT}
-chmod +x terralink${EXT}
+curl -L "$URL" -o terralink
+chmod +x terralink
+echo "$(pwd)/terralink" >> $GITHUB_PATH
 
